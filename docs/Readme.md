ec6c734fc0ba470735050416326269cde36d04bc
hyeon002 <hhj48770@naver.com>
2024-04-02 01:05:27 +0900

    react 과제

---
+++ docs/Readme.md
@@ -0,0 +1,4 @@
+**background와 background-image**
+- background 와 backgournd-image 는 모두 배경을 지정할 수 있다. 
+- But, background-image 의 경우 배경 사진만 변경할 수 있는 반면, 
+- background 는 다른 옵션들까지 변경 가능하다. 
---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Content from "./components/Content";
+import Signup from "./components/Signup";
+import styles from './App.css';
+
+
+function App() {
+  return (
+    <div className="total">
+        <Content ></Content>
+        <Signup></Signup>
+      </div>
+  );
+}
+
+export default App;
